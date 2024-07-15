db8e24a22d7b822efb8839ecbc3e8465cf863ec7
Ly Minh Hong <lyminhhong30122003@gmail.com>
2024-07-15 17:59:11 +0700

    lab06

---
+++ src/pages/AboutPage.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import "../css/AboutPage.css"
+
+function AboutPage() {
+    return (
+        <div>
+            <h1 className='introduce'> Lý Minh Hồng - CE171170 - SE1717 -FER202 - SU24 </h1>
+        </div>
+    )
+}
+
+export default AboutPage
